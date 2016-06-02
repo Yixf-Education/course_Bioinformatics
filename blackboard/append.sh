2374175fd0b3ac6892e17eb36737fcad270909d4
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2016-06-02 16:50:01 +0800

    Add materials for 2013-2014-2

--- blackboard/append.sh
+++
@@ -1,2 +0,0 @@
-convert -append gene1.png gene2.png gene.png
-convert -append genome1.png genome2.png genome.png
---
+++ example/503_galaxy/liftOver/liftover.sh
@@ -0,0 +1,1 @@
+./liftOver ../chr22_hg19_gene.bed hg19ToHg18.over.chain.gz hg18.map hg18.unmap
